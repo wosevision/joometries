f352e725e4d89cc8bc8b353f6e24167adef21eaa
Wose <Jacksonteather@hotmail.com>
2016-10-24 16:30:42 -0400

    Improved Point class
    Added push() overwrite that adds new Points to a new MultiPoint instead of onto the Point itself; tests added

--- src/Point.js
+++ src/Point.js
@@ -1,4 +1,5 @@
 import { JoomArray } from './JoomArray';
+import { MultiPoint } from './MultiPoint';
 import { ErrorMaker, ERRORS } from './utils/errors';
 
 class Point extends JoomArray {
@@ -8,23 +9,27 @@ class Point extends JoomArray {
 		makeError.if(args.length === 0, ERRORS.VALUE_MISSING);
 		// super(fn, ...args) a new JoomArray with a flatmap function
 		super(
-				// force matches to numbers and return them
-				arg => typeof arg === 'number' 
-					// if arg is a number, it's good to return
-					? [ arg ]
-					: arg
-					// if not, convert it to a string...
-					.toString()
-					// ...scour it for any semblance of a signed decimal...
-					.match(/(\-?\d+(\.\d+)?)/g)
-					// ...and force all matches to number or zero
-					.map(item => ( Number(item) || 0 ) ),
+			// force matches to numbers and return them
+			arg => typeof arg === 'number' 
+				// if arg is a number, it's good to return
+				? [ arg ]
+				: arg
+				// if not, convert it to a string...
+				.toString()
+				// ...scour it for any semblance of a signed decimal...
+				.match(/(\-?\d+(\.\d+)?)/g)
+				// ...and force all matches to number or zero
+				.map(item => ( Number(item) || 0 ) ),
 			...args
 		);
 		// error if no matches
 		makeError.if(!this, ERRORS.NO_MATCHES);
 		// error if final point has more than 3 coords
 		makeError.if(this.length !== 3, ERRORS.NOT_ENOUGH_VAL);
+	}
+	push(...vals) {
+		const point = new Point(vals);
+		return new MultiPoint(this, point);
 	}
 }
 
--- test/unit/Point.js
+++ test/unit/Point.js
@@ -1,4 +1,4 @@
-import { Point } from '../../src';
+import { Point, MultiPoint } from '../../src';
 import { JoomError } from '../../src/utils/errors';
 
 describe('Point', () => {
@@ -11,56 +11,66 @@ describe('Point', () => {
 	  	point = new Point(11, 22, 33);
 	  });
 
-    it('should be defined', function() {
+    it('should be defined', () => {
         expect(point).to.exist;
     });
 
-    it('should return an instance of Point', function() {
+    it('should return an instance of Point', () => {
     	expect(point).to.be.instanceof(Point);
     });
 
-    it('should have a length property with a value of 3', function() {
+    it('should have a length property with a value of 3', () => {
 			expect(point).to.have.ownProperty('length');
 			expect(point).to.have.lengthOf(3);
     });
 
-    it('should have a working push() function', function() {
-    	point.push(44);
-			expect(point).to.deep.equal([11,22,33,44])
+    it('should have working array mechanics', () => {
+    	expect(point.push).to.be.a('function');
+    	expect(point.length).to.be.a('number');
     });
 
-    it('should error if no arguments provided', function() {
+  });
+
+  describe('Validation and errors', () => {
+
+    it('should error if no arguments provided', () => {
     	const errorProne = () => {
     		console.log(new Point());
     	}
     	expect(errorProne).to.throw('no values provided for point');
     });
 
-    it('should error if insufficient arguments provided', function() {
+    it('should error if insufficient arguments provided', () => {
     	const errorProne = () => {
     		console.log(new Point(11, 22));
     	}
     	expect(errorProne).to.throw('invalid number of values for point');
     });
 
-  });
+    it('should push new Points into a new MultiPoint instead of onto the Point', () => {
+    	const newPoint = point.push(44, 55, 66);
+    	expect(newPoint).to.be.instanceof(MultiPoint);
+    	expect(newPoint).to.deep.equal([[ 11, 22, 33 ], [ 44, 55, 66 ]]);
+    });
+
+	});
 
   describe('String matching', () => {
 
-    it('should parse an easy, well-formatted string', function() {
+    it('should parse an easy, well-formatted string', () => {
   		point = new Point('11, 22, 33');
       expect(point).to.deep.equal([11,22,33]);
     });
 
-    it('should tolerate wacky spacing', function() {
+    it('should tolerate wacky spacing', () => {
   		point = new Point('  11 , 22   , 33   ');
       expect(point).to.deep.equal([11,22,33]);
     });
 
-    it('should recover from mixing strings with numbers', function() {
-  		let point1 = new Point('11', 22, 33);
-  		let point2 = new Point('11, 22', 33);
-  		let point3 = new Point(11, '22', '33');
+    it('should recover from mixing strings with numbers', () => {
+  		const point1 = new Point('11', 22, 33);
+  		const point2 = new Point('11, 22', 33);
+  		const point3 = new Point(11, '22', '33');
       expect(point1).to.deep.equal([11,22,33]);
       expect(point2).to.deep.equal([11,22,33]);
       expect(point3).to.deep.equal([11,22,33]);
@@ -69,19 +79,19 @@ describe('Point', () => {
   });
 
   describe('Recovery from bad formatting', () => {
-    it('should accept an already-initialized Point', function() {
-  		let newPoint = new Point(point);
+    it('should accept an already-initialized Point', () => {
+  		const newPoint = new Point(point);
       expect(newPoint).to.deep.equal([11,22,33]);
     });
-    it('should tolerate infinite levels of inappropriately nested arrays', function() {
-  		let point1 = new Point([11, 22, 33]);
-  		let point2 = new Point([[11, 22], 33]);
-  		let point3 = new Point([[11], [[22], 33]]);
+    it('should tolerate infinite levels of inappropriately nested arrays', () => {
+  		const point1 = new Point([11, 22, 33]);
+  		const point2 = new Point([[11, 22], 33]);
+  		const point3 = new Point([[11], [[22], 33]]);
       expect(point1).to.deep.equal([11,22,33]);
       expect(point2).to.deep.equal([11,22,33]);
       expect(point3).to.deep.equal([11,22,33]);
     });
-    it('should recover from basically any abuse', function() {
+    it('should recover from basically any abuse', () => {
   		point = new Point([[11, ['22']], '33abc']);
       expect(point).to.deep.equal([11,22,33]);
     });
